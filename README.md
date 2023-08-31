44676f654d2978242a956e4c6020f732e38d8a0d
sixpacksoda123 <55291477+sixpacksoda123@users.noreply.github.com>
2023-08-31 15:53:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# et720-2023-chrisa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/gzbw9p-1zrnba)
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    JumbotronComponent,
+    FooterComponent,
+    SectionComponent,
+    HeaderComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
